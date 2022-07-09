1c34c235f7df7c2baeb1cfba6b8103099e4f6526
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-09 16:56:51 +0500

    006 Задача4

---
+++ C#006/Program.cs
@@ -0,0 +1,5 @@
+﻿string[] i = Console.ReadLine().Split(' ');
+int a = Convert.ToInt32(i[0]);
+int b = Convert.ToInt32(i[1]);
+int c = Convert.ToInt32(i[2]);
+Console.WriteLine(Math.Max(a, Math.Max(b, c)));
