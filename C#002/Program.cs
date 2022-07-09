e603970d97e7206e4565c6ba640dc81b4bc4f53d
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-09 14:11:07 +0500

    002

---
+++ C#002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число, ");
+int namber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число, ");
+int namber = Convert.ToInt32(Console.ReadLine());
+
+if (squareNamber == namber * namber);
+{
+    Console.WriteLine("Число " + squareNamber +  " является квадратом " + namber);
+}
+else
+{
+    Console.WriteLine("Число " + squareNamber +  " НЕ является квадратом " + namber);
+}
