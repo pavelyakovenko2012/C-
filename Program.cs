a757048fb16fe275fbd772cd737f84878ada7721
Pavel Yakovenko <107810250+pavelyakovenko2012@users.noreply.github.com>
2022-07-09 22:22:40 +0500

    Площадь круга

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿System.Console.WriteLine("Введите радиус круга: ");
+double radius = Convert.ToDouble(Console.ReadLine());
+double area = Math.PI * Math.Pow(radius, 2);
+System.Console.WriteLine("Площадь круга с радиусом {0} равна {1}", radius, area);
