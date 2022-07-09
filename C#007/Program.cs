045505b2210fe9784764274bea76d9a64755615f
Pavel Yakovenko <107810250+pavelyakovenko2012@users.noreply.github.com>
2022-07-09 22:25:57 +0500

    Delete Program.cs

--- C#007/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine("Введите число : ");
-int a = Convert.ToInt32(Console.Read());
-if (a % 2 == 0) Console.WriteLine("Четное");
-else Console.WriteLine("Нечетное");
-Console.ReadKey();
