c07e1fd7d012bd3ff307c5265dc6a0b5f549d291
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-09 15:07:09 +0500

    003

---
+++ C#003/Program.cs
@@ -0,0 +1,36 @@
+﻿int dayNamber = Convert.ToInt32(Console.ReadLine());
+if (dayNamber>=1 && dayNamber <=7 )
+{
+    if (dayNamber == 1 )
+    {
+         Console.WriteLine("Понедельник");
+    }
+    if (dayNamber == 2 )
+    {
+         Console.WriteLine("Вторник");
+    }
+    if (dayNamber == 3 )
+    {
+         Console.WriteLine("Среда");
+    }
+    if (dayNamber == 4 )
+    {
+         Console.WriteLine("Четверг");
+    }
+    if (dayNamber == 5 )
+    {
+         Console.WriteLine("Пятница");
+    }
+    if (dayNamber == 6 )
+    {
+         Console.WriteLine("Суббота");
+    }
+    if (dayNamber == 7 )
+    {
+         Console.WriteLine("Воскресенье");
+    }
+}
+else
+{
+     Console.WriteLine("Error 404, day not found");
+}
