5a30bc2c992ee33a8200e49c1ef4e39ade04d306
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-09 17:11:29 +0500

    007 Задача8

---
+++ C#007/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число : ");
+int a = Convert.ToInt32(Console.Read());
+if (a % 2 == 0) Console.WriteLine("Четное");
+else Console.WriteLine("Нечетное");
+Console.ReadKey();
